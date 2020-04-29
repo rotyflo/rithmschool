63ad1e80a17c558f5dfd4d6de6a52e0cdcdae77a
RT <rotyflo@gmail.com>
2020-04-28 23:33:36 -0700

    Submit intermediate terminal exercises answers

---
+++ terminal/intermediate/part1.sh
@@ -0,0 +1,50 @@
+# Create an environment variable called FIRST_NAME and set it equal to your first name (this does not need to be permanent)
+export FIRST_NAME='rt'
+
+# Print the FIRST_NAME variable
+echo $FIRST_NAME
+
+# Print out the $PATH variable
+echo $PATH
+
+# What is the $PATH variable?
+# The location of the programs called in the terminal
+
+# Why would you want to create an environment variable?
+# To reference something easily later on
+
+# How do you permanently save environment variables?
+# Write them to your shell config file (.bashrc for me)
+
+# What is a process?
+# A program that is currently running
+
+# How do you list all processes running on your machine?
+ps aux
+
+# What is a PID?
+# process identifier
+
+# How do you terminate a process?
+# kill <pid>
+
+# What is the difference between kill and kill -9?
+# kill sends a TERM signal, kill -9 sends a more aggressive KILL signal
+
+# What grep flag allows for case insensitive search?
+# -i
+
+# What grep flag allows for a certain number of lines before the match?
+# -B
+
+# What grep flag allows for a certain number of lines around the match?
+# -C
+
+# What grep flag allows for a certain number of lines after the match?
+# -A
+
+# What grep flag allows for full word search?
+# -w
+
+# What grep flag shows you the line number of a match?
+# -n
