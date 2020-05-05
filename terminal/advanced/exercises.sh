7fdf474fa0fd0fff1dbe163c644aa48c5ffb4bab
RT <rotyflo@gmail.com>
2020-05-05 10:09:53 -0700

    Correct answers for advanced exercises

--- terminal/advanced/exercises.sh
+++ terminal/advanced/exercises.sh
@@ -1,14 +1,14 @@
 # Replace all of the - with : using sed
-sed 's/-/:/g' favorite_foods.txt
+sed -ie 's/-/:/g' favorite_foods.txt
 
 # Return a file with just the first name and last name separated by a space (you can do this with cut and sed or just sed)
 cut -d - -f 1,2 favorite_foods.txt | sed 's/-/ /g'
 
 # Using cut print out just the numbers 2, 3, 4, 5. Use xargs to print them all on 1 line
-cut -d '>' -f 5 numbers.txt | xargs echo
+cut -c 6 numbers.txt | xargs
 
 # Using xargs in the ./Desktop directory, find all of the files that include the text Welcome
-find ./Desktop -name '*' | xargs grep 'Welcome'
+find ~/Desktop -name '*' | xargs grep 'Welcome'
 
 # Write a shell script called access_file.sh which accepts one parameter and changes the permissions to 755
 echo 'chmod 755 $1' > access_file.sh
